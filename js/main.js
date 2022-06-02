f75a2f9c57191005544a1adbeeb8db2e9c8bdca7
Vanessa <vc1530@nyu.edu>
2022-06-02 17:15:04 -0400

    finished about and projects page

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function () {
+
+    $(".project").mouseover(function() { 
+        const desc = $(this).find(".project-desc"); 
+        desc.css("background-color", "var(--sinbad"); 
+        desc.css("color", "white"); 
+        const img = $(this).find("img.project-img"); 
+        img.css("filter", "blur(4px)")
+        img.css("-webkit-filter", "blur(4px)");
+        const icon = $(this).find(".img-container i"); 
+        icon.css("display", "block"); 
+    })
+
+    $(".project").mouseout(function() { 
+        const desc = $(this).find(".project-desc"); 
+        desc.css("background-color", "var(--almond"); 
+        desc.css("color", "var(--text-color)"); 
+        const img = $(this).find("img.project-img"); 
+        img.css("filter", "none")
+        img.css("-webkit-filter", "none"); 
+        const icon = $(this).find(".img-container i"); 
+        icon.css("display", "none"); 
+    })
+
+  })
